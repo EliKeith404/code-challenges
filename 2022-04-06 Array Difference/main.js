b7cde344f07cd58333eeaeb6e7aa35ae6adff210
elikeith404 <ekbass44@gmail.com>
2022-04-07 19:32:48 -0400

    Code Challenge - Array Difference - Solution

--- 2022-04-06 Array Difference/main.js
+++ 2022-04-06 Array Difference/main.js
@@ -11,13 +11,11 @@ If a value is present in b, all of its occurrences must be removed from the othe
 arrayDiff([1,2,2,2,3],[2]) == [1,3]
 */
 
-
 function arrayDiff(a, b) {
     // Every element in 'a' runs through a filter. 
     // If the element is not an index of 'b', add it to the array.
-
     return (a.filter(x => b.indexOf(x) === -1));
 
     // Alternate solution: If the element in 'a' is not included in 'b', add it to the array.
     // return (a.filter( x => !b.includes(x)));
-  }
+}
