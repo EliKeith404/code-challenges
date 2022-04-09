278cc16fabc9d98003519b36ae11526164e76919
elikeith404 <ekbass44@gmail.com>
2022-04-08 23:01:25 -0400

    Coding Challenge - Count Vowels in String - Solution using 'for of' loop

---
+++ 2922-04-08 String Vowel Count/main.js
@@ -0,0 +1,20 @@
+/* 
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+function getCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']; 
+    let vowelsCount = 0;
+    
+    for (let c of str){
+      if (vowels.includes(c)){
+        vowelsCount++;
+      }
+    }
+    
+    return vowelsCount;
+}
