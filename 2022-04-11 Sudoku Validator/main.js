1016c4f3ca7b7ac619769e4e735938e906065e4e
elikeith404 <ekbass44@gmail.com>
2022-04-19 02:36:37 -0400

    Finished Solution, brute force method

--- 2022-04-11 Sudoku Validator/main.js
+++ 2022-04-11 Sudoku Validator/main.js
@@ -1,33 +1,53 @@
 function validSolution(board){
-    // Check rows
-    for (let e of board){
-      if ((new Set(e)).size !== e.length) return false;
-      if (e.includes(0)) return false;
+
+  let threeByThree = [];
+  
+  for (let j=0; j<board.length; j++){
+    
+    // Check Rows
+    if ((new Set(board[j])).size !== board[j].length) return false;
+    // Check if board contains 0
+    if (board[j].includes(0)) return false;
+    
+    let column = [];
+    
+    for (let i=0; i<board.length; i++){
+      
+      // Fill array with column values
+      column.push(board[i][j]);
+      
+      // Fill array with 3x3 (Kinda rough but I couldn't get my other thoughts working)
+      if ((i===0 && (j===0 || j===3 || j===6)) 
+        ||(i===3 && (j===0 || j===3 || j===6))
+        ||(i===6 && (j===0 || j===3 || j===6))
+      ){
+        threeByThree.push(board[i][j]);
+        threeByThree.push(board[i][j+1]);
+        threeByThree.push(board[i][j+2]);
+        threeByThree.push(board[i+1][j]);
+        threeByThree.push(board[i+1][j+1]);
+        threeByThree.push(board[i+1][j+2]);
+        threeByThree.push(board[i+2][j]);
+        threeByThree.push(board[i+2][j+1]);
+        threeByThree.push(board[i+2][j+2]);
+      }
+      
+      // Check 3x3
+      if (threeByThree.length === 9){
+        if (new Set(threeByThree).size !== threeByThree.length){
+          return false;
+        }
+        threeByThree = [];
+      }
+      
     }
     
     // Check Columns
-    for (let j=0; j<board.length; j++){
-      let column = [];
-      for (let i=0; i<board.length; i++){
-        column.push(board[i][j]);
-      }
-      console.log(column);
-      if ((new Set(column)).size !== column.length) {
-        return false;
-      }
+    if ((new Set(column)).size !== column.length) {
+      return false;
     }
-    return true;
+  }
+  
+  // If the loops don't catch invalid boards, return true
+  return true;
 }
-
-
-console.log(validSolution[
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [2, 3, 1, 5, 6, 4, 8, 9, 7],
-    [3, 1, 2, 6, 4, 5, 9, 7, 8],
-    [4, 5, 6, 7, 8, 9, 1, 2, 3],
-    [5, 6, 4, 8, 9, 7, 2, 3, 1],
-    [6, 4, 5, 9, 7, 8, 3, 1, 2],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-    [8, 9, 7, 2, 3, 1, 5, 6, 4],
-    [9, 7, 8, 3, 1, 2, 6, 4, 5]
-]);
