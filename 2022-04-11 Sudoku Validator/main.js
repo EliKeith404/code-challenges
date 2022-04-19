da47652c33953ebc93eb3ea3fab4a58abb7d1b92
elikeith404 <ekbass44@gmail.com>
2022-04-19 02:37:58 -0400

    Added challenge rules

--- 2022-04-11 Sudoku Validator/main.js
+++ 2022-04-11 Sudoku Validator/main.js
@@ -1,3 +1,9 @@
+/* 
+Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+
+The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+*/
+
 function validSolution(board){
 
   let threeByThree = [];
