41d4a777b1fd94416eb6d6d2ef792f491003656b
elikeith404 <ekbass44@gmail.com>
2022-05-27 13:34:56 -0400

    Coding Challenge - Range Extraction - Solution

--- 2022-05-25 Range Extraction/main.js
+++ 2022-05-25 Range Extraction/main.js
@@ -8,7 +8,6 @@ or a range of integers denoted by the starting integer separated from the end in
 Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
 
 Example:
-
 solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
 // returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 */
