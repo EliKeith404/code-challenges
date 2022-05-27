8bfc1aa3bd29f37f6b50cd2d801faa5dc335abd8
elikeith404 <ekbass44@gmail.com>
2022-05-26 23:58:32 -0400

    Coding Challenge - Positive Count, Negative Sum

---
+++ 2022-05-27 Positive Count, Negative Sum/main.js
@@ -0,0 +1,25 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+    For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+    let positiveCount = 0;
+    let negativeSum = 0;
+    
+    if(!input || input.length === 0){
+      return [];
+    }
+    
+    for(let num of input){
+      num > 0 ? positiveCount++ : negativeSum += num;
+    }
+    
+    return [positiveCount, negativeSum]
+  }
