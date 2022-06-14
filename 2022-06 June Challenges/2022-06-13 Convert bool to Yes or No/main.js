4cfef1853054ec38ec6676c59002190e8335767a
elikeith404 <ekbass44@gmail.com>
2022-06-14 00:16:47 -0400

    Coding Challenge - Convert Bool to Yes or No - Solution

---
+++ 2022-06 June Challenges/2022-06-13 Convert bool to Yes or No/main.js
@@ -0,0 +1,9 @@
+/* 
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No";
+}
