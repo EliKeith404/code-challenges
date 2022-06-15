2b01ee66385bdacee115b236995c21ec4fb6c446
elikeith404 <ekbass44@gmail.com>
2022-06-15 00:18:49 -0400

    Coding Challenge - Convert Num to String - Solution

---
+++ 2022-06 June Challenges/2022-06-14 Convert Num to String/main.js
@@ -0,0 +1,15 @@
+/* 
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+    123 --> "123"
+    999 --> "999"
+*/
+
+function numberToString(num) {
+    return num.toString();
+}
