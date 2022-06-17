a9843ae89e4ad09ee7dc861af81944764053bda3
elikeith404 <ekbass44@gmail.com>
2022-06-17 00:43:34 -0400

    Added alternate solution

--- 2022-06 June Challenges/2022-06-15 String Repeat/main.js
+++ 2022-06 June Challenges/2022-06-15 String Repeat/main.js
@@ -1,6 +1,23 @@
 /* 
+https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
 
+Write a function called repeatStr which repeats the given string string exactly n times.
 
-
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
 */
 
+function repeatStr (n, s) {
+    let newStr = '';
+    
+    for(let i=0; i<n; i++){
+      newStr += s;
+    }
+    
+    return newStr;
+}
+
+// Alternate Solution
+function repeatStr(n,s){
+  return s.repeat(n);
+}
