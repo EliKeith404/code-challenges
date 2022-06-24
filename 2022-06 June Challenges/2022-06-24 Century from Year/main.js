cf262802d004c3d5fafb0bb5795d6b0d60792c47
elikeith404 <ekbass44@gmail.com>
2022-06-24 15:26:35 -0400

    Coding Challenge - Century From Year - Solution

---
+++ 2022-06 June Challenges/2022-06-24 Century from Year/main.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+    1705 --> 18
+    1900 --> 19
+    1601 --> 17
+    2000 --> 20
+*/
+
+function century(year) {
+    return year%100 > 0 ? Math.floor(year/100+1) : year/100;
+}
+
+// Shorter
+function century(year) {
+    return Math.ceil(year/100);
+  }
