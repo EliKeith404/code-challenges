e3c966da62d8a9459fcc5c99eabf4eaa2acb8789
elikeith404 <ekbass44@gmail.com>
2022-06-26 17:14:49 -0400

    Coding Challenge - Name Abbreviations - Solution

---
+++ 2022-06 June Challenges/2022-06-25 Name Abbreviation/main.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+    Sam Harris => S.H
+    patrick feeney => P.F
+*/
+
+function abbrevName(name){
+    let splitName = name.split(' ');
+    return `${splitName[0][0]}.${splitName[1][0]}`;
+}
