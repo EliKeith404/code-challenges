91685616469644f45b45a4f2e4dbdbaf8bf8d74a
elikeith404 <ekbass44@gmail.com>
2022-06-26 17:18:44 -0400

    Fixed lowercase inputs to always output uppercase abbreviations

--- 2022-06 June Challenges/2022-06-25 Name Abbreviation/main.js
+++ 2022-06 June Challenges/2022-06-25 Name Abbreviation/main.js
@@ -12,5 +12,5 @@ It should look like this:
 
 function abbrevName(name){
     let splitName = name.split(' ');
-    return `${splitName[0][0]}.${splitName[1][0]}`;
+    return `${splitName[0][0].toUpperCase()}.${splitName[1][0].toUpperCase()}`;
 }
