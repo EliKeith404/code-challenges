58637207475685f7d8b30a858292476b7020d68e
elikeith404 <ekbass44@gmail.com>
2022-08-05 00:24:15 -0400

    Coding Challenge - Unique in Order - Solution

---
+++ 2022-07 July Challenges/2022-07-02 Unique in Order/main.js
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+  uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+  uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+  uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
+
+var uniqueInOrder = function (iterable) {
+	let result = [];
+
+	for (let i = 0; i < iterable.length; i++) {
+		if (iterable[i] !== iterable[i + 1]) {
+			result.push(iterable[i]);
+		}
+	}
+
+	return result;
+};
